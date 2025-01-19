07ff90ee435cb062ad82d4c9e37f12188007d880
Leif <squall140@gmail.com>
2025-01-19 10:34:37 +0800

    feat: request params support for star pages

--- mock/question.js
+++ mock/question.js
@@ -5,6 +5,7 @@ const Random = Mock.Random
 
 module.exports = [
     {
+        // Retrieve a question details by id
         url: '/api/question/:id',
         method: 'get',
         response(){
@@ -25,5 +26,36 @@ module.exports = [
                 // msg: 'error 测试'
             }
         }
-    }
+    },
+    {
+        // publish a question
+        url: '/api/question',
+        method: 'post',
+        response(){
+            return {
+                errno: 0,
+                data: {
+                    id: Random.id()
+                }
+            }
+        }
+    },
+    {
+        // get question list
+        url: '/api/question',
+        method: 'get',
+        response(ctx){
+            const { url ='' } = ctx
+            // if url includes isStar, return isStar = true
+            const isStar = url.includes('isStar') ? true : false
+            console.log(isStar)
+            return {
+                errno: 0,
+                data: {
+                    list: getQuestionList({isStar}),
+                    total: 100
+                }
+            }
+        }
+    },
 ]
